edc926e400592c0d3c9fe79635f061f60f583b5e
thoriqadillah <thoriqadillah59@gmail.com>
2021-05-21 17:31:25 +0700

    menambahkan model pada tiap tabel

--- app/Controllers/Highscore.php
+++ app/Controllers/Highscore.php
@@ -2,12 +2,21 @@
 
 namespace App\Controllers;
 use App\Models\Users_model;  //mengambil Model pada folder Model dengan menggunakan namespace
+use App\Models\Admin_model;  
+use App\Models\Post_model;  
+use App\Models\Games_model;  
 
 class Highscore extends BaseController {
 	protected $users_model;
+	protected $admin_model;
+	protected $post_model;
+	protected $games_model;
 
 	public function __construct() {
 		$this->users_model = new Users_model(); //untuk memanggil model sekali dan bisa digunakan berkali2
+		$this->admin_model = new Admin_model();
+		$this->post_model = new Post_model();
+		$this->games_model = new Games_model();
 	}
 
 	public function index() {
