a0ea521e804c7f90135477201e2e905129d2d4a8
thoriqadillah <thoriqadillah59@gmail.com>
2021-05-21 07:36:18 +0700

    menambahkan login_process

--- app/Controllers/Highscore.php
+++ app/Controllers/Highscore.php
@@ -6,9 +6,13 @@ use App\Models\Highscore_model;
 
 class Highscore extends BaseController {
 	protected $highscore;
+	protected $session;
+	protected $rrequest;
 
 	public function __construct() {
 		$this->highscore = new Highscore_model(); //untuk memanggil model sekali dan bisa digunakan berkali2
+		$this->session = \Config\Services::session();
+		$this->rrequest = service('request');
 	}
 
 	public function index() {
@@ -17,8 +21,6 @@ class Highscore extends BaseController {
 			//untuk memanggil model bisa dilempar ke $data sebagai key dan value, kemudian dikirim data ke view dan dilooping menggunakan foreach
 			//'namaVar' => '$this->namaModel->namaMethod();'
 		];
-
-		
 		// return view('index', $data); //memanggil view dengan mengirimkan data
 		return view('index', $data);
 	}
@@ -31,12 +33,13 @@ class Highscore extends BaseController {
 	}
 	
 	public function login_process() {
-		dd($this->request->('email'));
-		$data = [
-			'title' => 'Home'
-		];
-		
-		return view('login', $data);
+		$email = 'coba@coba.com';
+		$pass = 'coba';
 
+		if ($this->highscore->can_login_user($email, $pass) && $this->session->get('logged_in')) {
+			return redirect()->to('home');
+		}
+
+		return redirect()->to('login');
 	}
 }
