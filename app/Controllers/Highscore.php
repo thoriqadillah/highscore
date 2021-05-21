6ef51fad926236011a323c6f4fcb879e8db50ce8
thoriqadillah <thoriqadillah59@gmail.com>
2021-05-21 21:11:19 +0700

    menambahkan template buat login user dan admin

--- app/Controllers/Highscore.php
+++ app/Controllers/Highscore.php
@@ -19,6 +19,7 @@ class Highscore extends BaseController {
 		$this->games_model = new Games_model();
 	}
 
+	//ini buat halaman tamu
 	public function index() {
 		$data = [
 			'title' => 'Home' //sebagai judul page, dikirim ke <title> pada view
@@ -29,5 +30,35 @@ class Highscore extends BaseController {
 		
 		// return view('index', $data); //memanggil view dengan mengirimkan data
 		return view('index', $data);
+	}
+
+	//buat validasi aja, misal kalo belum login nanti gabisa upload, harus login dulu
+    public function upload() {
+        //misal user pengen upload, cek dulu dia udah logged_in belum di session nya
+        //jika logged in true, yaudah diarahin ke halaman upload
+        //kalo false ke halaman login
+    }
+	
+	//buat nampilin halaman home buat user
+	public function home() {
+		$data = [
+			'title' => 'Home' //sebagai judul page, dikirim ke <title> pada view
+		];
+		//buat validasi lagi biar gaboleh masuk kalo misal maksa masuk halaman controller/function lewat url bar
+		
+		//cek dulu dia udah logged_in belum di session nya
+		//jika logged in true, yaudah diarahin ke halaman home buat user
+		//kalo false ke halaman login
+	
+	}
+
+	//buat nampilin halaman admin
+	public function admin() {
+		$data = [
+			'title' => 'Home' //sebagai judul page, dikirim ke <title> pada view
+		];
+		//ini juga dibuat validasinya juga ya, sama kaya user
+
+
 	}
 }
---
+++ app/Controllers/Login.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\Users_model;  //mengambil Model pada folder Model dengan menggunakan namespace
+use App\Models\Admin_model;  
+
+class Highscore extends BaseController {
+	protected $users_model;
+	protected $admin_model;
+
+	public function __construct() {
+		$this->users_model = new Users_model(); //untuk memanggil model sekali dan bisa digunakan berkali2
+		$this->admin_model = new Admin_model();
+	}
+
+    //tampilin halaman login
+	public function index() {
+		$data = [ //jangan lupa $data nanti dikirimkan ke return view ya
+			'title' => 'Home' 
+		];
+
+        
+	}
+
+    //buat ngeproses login
+    public function signin() {
+        //tutorial, kalo udah selesai mungkin bisa dihapus, atau dicut dimana gitu
+        //ambil kiriman data email itu buat email user atau username admin
+        //ambil juga passwordnya. Jadi nanti action diarahin ke sini
+
+        //manfaatin function can_login dari user_model sama admin_model
+        //kalo hasilnya true buat function user, redirect ke halaman home buat user (index buat tamu)
+        //kalo hasilnya true buat function user, redirect ke halaman admin yang nanti dibuat dila
+
+        //untuk nanti buat ngakses halaman admin, nanti buat controller admin lagi ya. Buat user pake highscore aja controllernya
+    }
+
+    
+}
