73804528b219d93156c6c266e41f8342780c5417
nadyafadhila18 <64628797+nadyafadhila18@users.noreply.github.com>
2021-05-23 13:12:53 +0700

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -2,18 +2,23 @@
 
 <?= $this->section('content'); ?>
 
-<div class="row">
-    <div class="col">
-        <form action="/highscore/login_process" method="POST">
-            <div class="form-group" class="text-center" >      
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" >
-            </div>
-            <div class="form-group">
-                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="password">
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
+<div class="container" align="center">
+    <h1>World's Rank Game</h1>
+    <form >
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Username</label>
+        <input type="username" class="form-control" id="exampleInputEmail1" aria-describedby="usernameHelp">
+        
+      </div>
+      <br> 
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      
+      <br>
+      <button type="submit" class="btn btn-primary">Log In</button>
+    </form>
 </div>
 
 <?= $this->endSection(); ?>
