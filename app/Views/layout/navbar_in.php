8d1ce42f01f6331ca7c50eabf3929f999a297bbf
thoriqadillah <thoriqadillah59@gmail.com>
2021-05-24 10:38:36 +0700

    menambahkan link untuk home

--- app/Views/layout/navbar_in.php
+++ app/Views/layout/navbar_in.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#">Leaderboard</a>
+        <a class="navbar-brand" href="/highscore/home">Leaderboard</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
