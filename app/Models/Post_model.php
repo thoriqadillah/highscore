b381273cbf6f2a2738518283cb05a39769d0e586
thoriqadillah <thoriqadillah59@gmail.com>
2021-05-28 13:50:51 +0700

    update post model untuk menampilkan post

--- app/Models/Post_model.php
+++ app/Models/Post_model.php
@@ -6,6 +6,13 @@ class Post_model extends Model {
     protected $table      = 'post'; //diisi nama tabel sesuai keinginan
     protected $useTimestamps = true;
     protected $allowedFields = ['image', 'score', 'user_id'];
+    protected $builder;
+    protected $db;
+
+    public function __construct() {
+        $this->db      = \Config\Database::connect();
+        $this->builder = $this->db->table('post');
+    }
     
     public function getTable($slug = false) { //semisal untuk mengambil row spesifik tabel, bisa menggunakan slug (jika ada)
         if ($slug == false) { //jika tidak ada, maka kembalikan semua isi tabel
@@ -16,6 +23,17 @@ class Post_model extends Model {
         
         //atau, jika ingin mengembalikan single row dengan id nya, parameternya bisa diganti dengan id, kemudian
         //$this->find($id); 
+    }
+
+    public function get_post($where) {
+        $this->builder->select('image', 'score', 'user_email', 'game_id')
+        ->join('users', 'user.id = post.user_id')
+        ->join('games', 'games.id = post.game_id')
+        ->where($where)
+        ->orderBy('score', 'DESC');
+        $query = $this->builder->get();
+
+        return $query;
     }
 
 }
