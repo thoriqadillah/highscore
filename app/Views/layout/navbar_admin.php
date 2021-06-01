a21f7bcb026a33004dda7a7dca431a9c1a8f6989
thoriqadillah <thoriqadillah59@gmail.com>
2021-06-01 14:40:31 +0700

    membenahi routing pada navbar_admin

--- app/Views/layout/navbar_admin.php
+++ app/Views/layout/navbar_admin.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="/admin/home">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/admin/index">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
