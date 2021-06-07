3f09fe46a5547a82c6b2d2cc469fc200f30f11e8
thoriqadillah <thoriqadillah59@gmail.com>
2021-06-07 13:30:02 +0700

    finishing part 2

--- app/Views/layout/navbar_admin.php
+++ app/Views/layout/navbar_admin.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="/admin/index">Admin</a>
+        <a class="navbar-brand" href="/admin">Admin</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="/admin/index">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/admin">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
--- app/Views/layout/navbar_in.php
+++ app/Views/layout/navbar_in.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="/highscore/home">Leaderboard</a>
+        <a class="navbar-brand" href="/home">Leaderboard</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -20,15 +20,15 @@
                     </div>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link btn btn-primary" role="button" href="/highscore/upload">Post</a>
+                    <a class="nav-link btn btn-primary" role="button" href="/upload">Post</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <?= $session_data['username']; ?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="/highscore/user_post">My Post</a>
-                        <a class="dropdown-item" href="/highscore/logout">Log out</a>
+                        <a class="dropdown-item" href="/user_post">My Post</a>
+                        <a class="dropdown-item" href="/logout">Log out</a>
                     </div>
                 </li>
             </ul>
